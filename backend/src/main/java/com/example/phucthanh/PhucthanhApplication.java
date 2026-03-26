90aa72feeab11013b701186c2597bf6567fbaa04
Trần Phúc Thành <tranphucthanh2002@gmail.com>
2026-03-26 10:24:23 +0700

    Fix: backend configuration and add CI/CD workflow

---
+++ backend/src/main/java/com/example/phucthanh/PhucthanhApplication.java
@@ -0,0 +1,13 @@
+package com.example.phucthanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhucthanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhucthanhApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/phucthanh/PhucthanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phucthanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhucthanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
